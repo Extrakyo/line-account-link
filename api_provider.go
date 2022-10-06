02ea928777aadf85e9dcc24a8769a08054c399c2
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-10-07 06:56:00 +0800

    123

--- api_provider.go
+++ api_provider.go
@@ -73,6 +73,12 @@ func login(w http.ResponseWriter, r *http.Request) {
 			// log.Printf("successful")
 			sNonce := generateNounce(token, name)
 			//update nounce to provider DB to store it.
+			db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/foodler")
+			if err != nil {
+				panic(err.Error())
+			}
+			defer db.Close()
+			insertStudent(sNonce)
 
 			//9. The web server redirects the user to the account-linking endpoint.
 			//10. The user accesses the account-linking endpoint.
@@ -138,4 +144,19 @@ func MD5(pw string) string {
 	algorithm := md5.New()
 	algorithm.Write([]byte(pw))
 	return hex.EncodeToString(algorithm.Sum(nil))
+}
+
+func insertStudent(studentName string) {
+	rs, err := db.Exec("INSERT INTO `user` (`Nounce`) VALUES (?)", studentName)
+	if err != nil {
+		log.Println(err)
+	}
+
+	rowCount, err := rs.RowsAffected()
+	rowId, err := rs.LastInsertId() // 資料表中有Auto_Increment欄位才起作用，回傳剛剛新增的那筆資料ID
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("新增 %d 筆資料，id = %d \n", rowCount, rowId)
 }
