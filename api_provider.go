ed226d30a2e42ed07de14b1de878a80a0c345630
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-10-07 06:47:12 +0800

    123

--- api_provider.go
+++ api_provider.go
@@ -1,95 +1,114 @@
 package main
 
 import (
-	b64 "encoding/base64"
+	"crypto/md5"
+	"database/sql"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // CustData : Customers data for provider website.
 type CustData struct {
 	ID     string
 	PW     string
-	Name   string
-	Age    int
-	Desc   string
 	Nounce string
 }
 
 var customers []CustData
 
 func init() {
-	//Init customer data in memory
-	customers = append(customers, []CustData{
-		{
-			ID:     "extra",
-			PW:     "Extra123",
-			Name:   "Tom",
-			Age:    43,
-			Desc:   "He is from A corp. likes to read comic books.",
-			Nounce: "",
-		},
-		{
-			ID:     "22",
-			PW:     "pw22",
-			Name:   "John",
-			Age:    25,
-			Desc:   "He is from B corp. likes to read news paper",
-			Nounce: "",
-		},
-		{
-			ID:     "33",
-			PW:     "pw33",
-			Name:   "Mary",
-			Age:    13,
-			Desc:   "She is a student, like to read science books",
-			Nounce: "",
-		},
-	}...)
+
 }
 
-// WEB: List all user in memory
+type Tag struct {
+	Username string
+	Password string
+	Nounce   string
+}
+
+var tags []Tag
+
 func listCust(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bookstore customer list as follow:\n")
-	for i, usr := range customers {
-		fmt.Fprintf(w, "%d \tID: %s \tName: %s \tPW: %s \tDesc:%s \n", i, usr.ID, usr.Name, usr.PW, usr.Desc)
-	}
+
 }
 
 // WEB: For login (just for demo)
+var db *sql.DB
+
 func login(w http.ResponseWriter, r *http.Request) {
-	//7. The user enters his/her credentials.
-	if err := r.ParseForm(); err != nil {
-		log.Printf("ParseForm() err: %v\n", err)
-		return
+
+	db, err := sql.Open("mysql", "canis:vz3s10cdDtkU1BRv@tcp(103.200.113.92)/foodler")
+	if err != nil {
+		panic(err.Error())
 	}
-	name := r.FormValue("user")
-	pw := r.FormValue("pass")
-	token := r.FormValue("token")
-	for i, usr := range customers {
-		if usr.ID == name {
-			if pw == usr.PW {
-				//8. The web server acquires the user ID from the provider's service and uses that to generate a nonce.
-				sNonce := generateNounce(token, name, pw)
+	defer db.Close()
 
-				//update nounce to provider DB to store it.
-				customers[i].Nounce = sNonce
+	results, err := db.Query("SELECT username, password FROM users WHERE identity = 'customer'")
+	if err != nil {
+		panic(err.Error())
+	}
 
-				//9. The web server redirects the user to the account-linking endpoint.
-				//10. The user accesses the account-linking endpoint.
-				//Print link to user to click it.
-				targetURL := fmt.Sprintf("https://access.line.me/dialog/bot/accountLink?linkToken=%s&nonce=%s", token, sNonce)
-				log.Println("generate nonce, targetURL=", targetURL)
-				tmpl := template.Must(template.ParseFiles("link.tmpl"))
-				if err := tmpl.Execute(w, targetURL); err != nil {
-					log.Println("Template err:", err)
-				}
-				return
+	for results.Next() {
+		// var user Tag
+		name := r.FormValue("user")
+		pw := r.FormValue("pass")
+		token := r.FormValue("token")
+
+		var user Tag
+		err = results.Scan(&user.Username, &user.Password)
+		if err != nil {
+			panic(err.Error())
+		}
+		PW := MD5(pw)
+		log.Printf(PW)
+
+		if user.Username == name && user.Password == PW {
+			//8. The web server acquires the user ID from the provider's service and uses that to generate a nonce.
+			// log.Printf("successful")
+			sNonce := generateNounce(token, name)
+			//update nounce to provider DB to store it.
+
+			//9. The web server redirects the user to the account-linking endpoint.
+			//10. The user accesses the account-linking endpoint.
+			//Print link to user to click it.
+			targetURL := fmt.Sprintf("https://access.line.me/dialog/bot/accountLink?linkToken=%s&nonce=%s", token, sNonce)
+			log.Println("generate nonce, targetURL=", targetURL)
+			tmpl := template.Must(template.ParseFiles("link.tmpl"))
+			if err := tmpl.Execute(w, targetURL); err != nil {
+				log.Println("Template err:", err)
 			}
+			return
 		}
 	}
+
+	// for i, usr := range customers {
+	// 	if usr.ID == name {
+	// 		if pw == usr.PW {
+	// 			//8. The web server acquires the user ID from the provider's service and uses that to generate a nonce.
+	// 			sNonce := generateNounce(token, name, pw)
+
+	// 			//update nounce to provider DB to store it.
+	// 			customers[i].Nounce = sNonce
+
+	// 			//9. The web server redirects the user to the account-linking endpoint.
+	// 			//10. The user accesses the account-linking endpoint.
+	// 			//Print link to user to click it.
+	// 			targetURL := fmt.Sprintf("https://access.line.me/dialog/bot/accountLink?linkToken=%s&nonce=%s", token, sNonce)
+	// 			log.Println("generate nonce, targetURL=", targetURL)
+	// 			tmpl := template.Must(template.ParseFiles("link.tmpl"))
+	// 			if err := tmpl.Execute(w, targetURL); err != nil {
+	// 				log.Println("Template err:", err)
+	// 			}
+	// 			return
+	// 		}
+	// 	}
+	// }
 	fmt.Fprintf(w, "Your input name or password error.")
 }
 
@@ -111,6 +130,12 @@ func link(w http.ResponseWriter, r *http.Request) {
 }
 
 // generate nonce (currently nounce combine by token + name + pw)
-func generateNounce(token, name, pw string) string {
-	return b64.StdEncoding.EncodeToString([]byte(token + name + pw))
+func generateNounce(token, name string) string {
+	return base64.StdEncoding.EncodeToString([]byte(token + name))
+}
+
+func MD5(pw string) string {
+	algorithm := md5.New()
+	algorithm.Write([]byte(pw))
+	return hex.EncodeToString(algorithm.Sum(nil))
 }
