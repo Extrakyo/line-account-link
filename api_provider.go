9968687a6a23041ffee64034695c17eb48cd8ee4
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-12-05 16:14:57 +0800

    123

--- api_provider.go
+++ api_provider.go
@@ -19,6 +19,7 @@ type CustData struct {
 	PW     string
 	Name   string
 	Nounce string
+	UserId string
 }
 
 var customers []CustData
@@ -84,7 +85,8 @@ func login(w http.ResponseWriter, r *http.Request) {
 				}
 				log.Println("id:", idAff)
 				if idAff == 0 {
-					_, err := db.Exec("INSERT INTO `linebot`(`nounce`, `username`, `password`, `name`) VALUES (?, ?, ?, ?)", sNonce, usr.ID, usr.PW, usr.Name)
+					usr.UserId = ""
+					_, err := db.Exec("INSERT INTO `linebot`(`nounce`, `username`, `password`, `userId`, `name`) VALUES (?, ?, ?, ?)", sNonce, usr.ID, usr.PW, usr.UserId, usr.Name)
 					if err != nil {
 						log.Println("exec failed:", err)
 					}
