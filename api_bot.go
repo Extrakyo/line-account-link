d1e78289626677eb62b12f334fa77cf030af81fa
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-12-05 18:23:22 +0800

    123

--- api_bot.go
+++ api_bot.go
@@ -83,7 +83,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				case strings.EqualFold(message.Text, "Un"):
 					for _, usr := range linkedCustomers {
 						if usr.LinkUserID == event.Source.UserID {
-							_, err := db.Exec("DELETE * FROM linebot WHERE `nounce` = ?", usr.Nounce)
+							_, err := db.Exec("DELETE FROM linebot WHERE `nounce` = ?", usr.Nounce)
 							if err != nil {
 								log.Println("exec failed:", err)
 								return
