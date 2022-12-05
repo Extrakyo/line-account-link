bde665e9eebd9de08a76b12796c76a0fabe3a2f2
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-12-05 18:43:06 +0800

    123

--- api_bot.go
+++ api_bot.go
@@ -13,9 +13,9 @@ import (
 // LinkCustomer : A chatbot DB to store account link information.
 type LinkCustomer struct {
 	//Data from CustData from provider.
-	Name   string
-	Nounce string
-	//For chatbot linked data.
+	ID         string
+	Name       string
+	Nounce     string
 	LinkUserID string
 }
 
@@ -150,15 +150,16 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						log.Println("exec failed:", err)
 						return
 					}
-					results, err := db.Query("SELECT `userId` FROM `linebot` WHERE `nounce` = ?", usr.Nounce)
+					results, err := db.Query("SELECT * FROM `linebot` WHERE `username` = ?", usr.ID)
 					if err != nil {
 						panic(err.Error())
 					}
 					var linkedUser LinkCustomer
 					for results.Next() {
-						results.Scan(&linkedUser.LinkUserID)
+						results.Scan(linkedUser.LinkUserID, &linkedUser.Nounce, linkedUser.Name)
 						linkedCustomers = append(linkedCustomers, linkedUser)
 					}
+					log.Println(linkedUser)
 					// linkedUser := LinkCustomer{
 					// 	Name:       usr.Name,
 					// 	LinkUserID: event.Source.UserID,
