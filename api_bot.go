187e5d00328fdd519283f10a2b15acb7c58d3d29
Extrakyo <43052494+Extrakyo@users.noreply.github.com>
2022-12-10 20:10:20 +0800

    123

--- api_bot.go
+++ api_bot.go
@@ -185,7 +185,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							rs.Scan(&ur.userID)
 						}
 						log.Println("USERID:" + ur.userID)
-						if ur.userID == event.Source.UserID {
+						if usr.LinkUserID == event.Source.UserID {
 							//取得訂單資料
 							rs, err := db.Query("SELECT `brandId`, `orderStatus`, `fullName`, `totalPrice` FROM `orderList` WHERE `username` = ? AND (`orderStatus` = 'isReceived' OR `orderStatus` = 'isPreparing') ORDER BY `needTime` DESC", usr.ID)
 							if err != nil {
